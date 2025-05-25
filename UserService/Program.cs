88b714fb0a551572d080548209f2ff6e2c8d1abb
RasmusOlesen00 <146640318+RasmusOlesen00@users.noreply.github.com>
2025-05-25 21:13:34 +0200

    authentication virker

--- UserService/Program.cs
+++ UserService/Program.cs
@@ -124,7 +124,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         options.TokenValidationParameters = new TokenValidationParameters()
         {
             ValidateIssuer = true,
-            ValidateAudience = true,
+            ValidateAudience = false,
             ValidateLifetime = true,
             ValidateIssuerSigningKey = true,
             ValidIssuer = builder.Configuration["JwtSettings:Issuer"],

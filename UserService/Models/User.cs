7cdcda2ceb80e21f990e342e90b152de56d01e53
Levi Emkjær <eaa23leej@students.eaaa.dk>
2025-05-26 10:03:40 +0200

    Opdatering af User model

--- UserService/Models/User.cs
+++ UserService/Models/User.cs
@@ -1,23 +1,31 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
-namespace UserService.Models{
-public class User
+namespace UserService.Models
 {
-    [BsonId]
-    [BsonRepresentation(BsonType.String)] // Eller BsonType.Binary hvis du vil gemme det binært
-    public Guid Id { get; set; }
-    public string? Username { get; set; } // Brugernavn
-    public string? Password { get; set; } // Adgangskode
-    public string? Role { get; set; } // Rolle (f.eks. "admin" eller "user")
-    public string? Name { get; set; }
-    public string? Address1 { get; set; }
-    public string? Address2 { get; set; }
-    public short PostalCode { get; set; }
-    public string? City { get; set; }
-    public string? EmailAddress { get; set; }
-    public string? PhoneNumber { get; set; }
-}
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)] // Eller BsonType.Binary hvis du vil gemme det binært
+        public Guid Id { get; set; }
+        public string? Username { get; set; } // Brugernavn
+        public string? Password { get; set; } // Adgangskode
+        public Role? Role { get; set; } // Rolle (f.eks. "admin" eller "user")
+        public string? Name { get; set; }
+        public string? Address1 { get; set; }
+        public string? Address2 { get; set; }
+        public short PostalCode { get; set; }
+        public string? City { get; set; }
+        public string? EmailAddress { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+
+    public enum Role
+    {
+        Admin,
+        User,
+        Customer
+    }
 
 }
 
